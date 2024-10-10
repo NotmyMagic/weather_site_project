3d570957eeb155921d20eefdb954e91e9c87b6e7
NotmyMagic <56935337+NotmyMagic@users.noreply.github.com>
2024-10-10 13:27:07 -0600

    added live news updates

--- script.js
+++ script.js
@@ -33,11 +33,55 @@ fetch(news_Key)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+
     let article = jsObject.articles;
 
-    for (let i = 0; i < article.length; i++) {
-      let article1 = document.createElement("article");
-      article1.className = "news-article";
-      let img;
-    }
+    let img0 = document.getElementById("header-img");
+    img0.src = article[0].urlToImage;
+
+    let title0 = document.getElementById("header-article");
+    title0.textContent = article[0].title;
+
+    let descript0 = document.getElementById("header-desc");
+    descript0.textContent = article[0].description;
+
+    // next article
+    let img1 = document.getElementById("article-img-1");
+    img1.src = article[1].urlToImage;
+
+    let title1 = document.getElementById("article-link-1");
+    title1.textContent = article[1].title;
+
+    let descript1 = document.getElementById("article-desc-1");
+    descript1.textContent = article[1].description;
+
+    // next article
+    let img2 = document.getElementById("article-img-2");
+    img2.src = article[2].urlToImage;
+
+    let title2 = document.getElementById("article-link-2");
+    title2.textContent = article[2].title;
+
+    let descript2 = document.getElementById("article-desc-2");
+    descript2.textContent = article[2].description;
+
+    // next article
+    let img3 = document.getElementById("article-img-3");
+    img3.src = article[3].urlToImage;
+
+    let title3 = document.getElementById("article-link-3");
+    title3.textContent = article[3].title;
+
+    let descript3 = document.getElementById("article-desc-3");
+    descript3.textContent = article[3].description;
+
+    // next article
+    let img4 = document.getElementById("article-img-4");
+    img4.src = article[4].urlToImage;
+
+    let title4 = document.getElementById("article-link-4");
+    title4.textContent = article[4].title;
+
+    let descript4 = document.getElementById("article-desc-4");
+    descript4.textContent = article[4].description;
   });
